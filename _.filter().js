ce0f1969861f37fbad821df831b2e23e32312dda
Kayode Oluwausi <66219016+KayodeOlusi@users.noreply.github.com>
2020-08-29 03:53:56 +0100

    Create _.filter().js
    A simple code demonstrating how to loop through an array of objects to filter out an output using the _.filter() function

---
+++ _.filter().js
@@ -0,0 +1,93 @@
+const _={};
+
+_.filter=function(arr,cb){
+const storage=[];
+
+for(let i=0;i<arr.length;i++){
+    if(cb(arr[i],i,arr)===true){
+        storage.push(arr[i])
+    }
+}
+return storage;
+}
+
+
+const videoData = [
+    {
+        name: 'Miss Scarlet',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Mrs. White',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Reverend Green',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Rusty',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Colonel Mustard',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Professor Plum',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    }
+];
+
+
+_.filter(videoData,function(suspectObject){
+    return suspectObject.present;
+});
