9955d25a9ea10e661cdc72a5572b9b3ac8d70f0f
Kayode Oluwausi <66219016+KayodeOlusi@users.noreply.github.com>
2020-08-29 02:37:53 +0100

    Update Functionals.js

--- Functionals.js
+++ Functionals.js
@@ -32,3 +32,12 @@ console.log(name,'is the oldest')
 })
 
 
+//Using _.map function
+const weapons=['candlesticks','lead pipe','revolver'];
+
+const makeBrown = function(item){
+return `broken ${item}`;
+}
+
+_.map(weapons,makeBrown); 
+
