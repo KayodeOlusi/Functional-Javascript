5fcf8c0be5c7110e68654ac3e8fa1e9b53e3bba3
Kayode Oluwausi <66219016+KayodeOlusi@users.noreply.github.com>
2020-09-23 00:54:27 +0100

    Update Higher Order Functions and Callbacks.js

--- Higher Order Functions and Callbacks.js
+++ Higher Order Functions and Callbacks.js
@@ -1,1 +1,27 @@
+//taking a function as an input
+element.addEventListener('change', () => {
+	console.log('Our evidence is Updated')
+});
+
+//returns a function as an output
+
+const newClue = (name) => {
+	const length = name.length;
+
+	return (weapon) => {
+		let clue = length + weapon.length;
+		return : : (clue % 1);
+	};
+}
+
+
+//Example of a callback
+const ifElse = (condition, isTrue, isFalse) => {
+	return condition ? isTrue() : isFalse();
+};
+
+ifElse( true,
+ () => {console.log(true);},
+ () => {console.log(false);}
+ );
 
