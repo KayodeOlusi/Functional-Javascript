9f1697f2356cb69d8350d4c904670b2a22d89967
Kayode Oluwausi <66219016+KayodeOlusi@users.noreply.github.com>
2021-01-13 13:10:03 +0100

    Create curry.js

---
+++ curry.js
@@ -0,0 +1,21 @@
+// How Currying in Javascript works
+var curry = (fn) => {
+    return (arg) => {
+        return (arg2) =>{
+      return fn(arg,arg2)      
+        }
+    }
+}
+
+// Practical Example
+
+var student = 
+          name =>
+                gender =>
+                      age => 
+                        name + ' '+ gender + ' ' + age 
+
+
+student('Hanna')('Female')('12')
+ 
+//log out Hanna Female 12 
