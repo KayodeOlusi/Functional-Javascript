4c3f29330eb503b77d079e8973ed5d7427b9e120
Kayode Oluwausi <66219016+KayodeOlusi@users.noreply.github.com>
2021-01-13 12:51:33 +0100

    Update Higher Order Functions and Callbacks.js

--- Higher Order Functions and Callbacks.js
+++ Higher Order Functions and Callbacks.js
@@ -45,3 +45,21 @@ const increment = n => {return n+1;};
 const square = n => {return n*n};
 
 const doThisMath = ( n, func ) => { return(func(n);};
+					   
+
+//					   
+function copy(array,instruction){
+var output  = [];
+for(i=0;i<array.length;i++){
+output.push(instruction(array[i]))
+}
+return output
+}
+
+var multiply = function(input){
+    return input*2
+};
+
+var result = copy([1,2,3],multiply)
+
+//result = [2,4,6]
