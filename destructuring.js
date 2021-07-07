05254f7b674fafcbe00a17993dbf1ef6f6495b62
KayodeOlusi <kayodeolusi@gmail.com>
2021-07-07 04:16:34 +0100

    added a new file for destructuring

---
+++ destructuring.js
@@ -0,0 +1,30 @@
+//demonstrating different ways of using destructuring
+
+//set your variables
+const a = 1;
+const b = 2;
+//use destructuring pattern to swap the values
+[a,b] = [b,a]
+// a = 2, b = 1
+
+
+//using the sperad operator (...)
+//set your variables
+const [a,...b] = [1,2,3]
+console.log(a,b)
+// a = 1, b = [2,3]
+
+//destructuring objects
+{
+    name: "alex",
+    age : 18,
+    sex : "male"
+}
+
+var {name,age,sex} = {
+    name: "alex",
+    age : 18,
+    sex : "male"
+}
+
+//name = alex, age = 18, sex = male
