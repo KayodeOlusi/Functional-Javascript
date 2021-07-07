aec3c2131f4b43f5872d40198c71cc35c3544f9c
KayodeOlusi <kayodeolusi@gmail.com>
2021-07-07 04:18:22 +0100

    Modified

--- Higher Order Functions and Callbacks.js
+++ Higher Order Functions and Callbacks.js
@@ -1,80 +1,92 @@
 //taking a function as an input
-element.addEventListener('change', () => {
-	console.log('Our evidence is Updated')
+element.addEventListener("change", () => {
+  console.log("Our evidence is Updated");
 });
 
 //returns a function as an output
 
 const newClue = (name) => {
-	const length = name.length;
+  const length = name.length;
 
-	return (weapon) => {
-		let clue = length + weapon.length;
-		return : : (clue % 1);
-	};
-}
-
+  return (weapon) => {
+    let clue = length + weapon.length;
+    return clue % 1;
+  };
+};
 
 //Example of a callback
 const ifElse = (condition, isTrue, isFalse) => {
-	return condition ? isTrue() : isFalse();
+  return condition ? isTrue() : isFalse();
 };
 
-ifElse( true,
- () => {console.log(true);},
- () => {console.log(false);}
- );
-
+ifElse(
+  true,
+  () => {
+    console.log(true);
+  },
+  () => {
+    console.log(false);
+  }
+);
 
 //ES6 Challenge{translate this to ES6}
-const increment = function(n){ return n + 1; };
+const increment = function (n) {
+  return n + 1;
+};
 
-const square = function(n){return n*n;};
+const square = function (n) {
+  return n * n;
+};
 
-const doThisMath = function(n,func){return func(n);};
-
+const doThisMath = function (n, func) {
+  return func(n);
+};
 
 //another example
-function mapWith(array,callback){
-    var output = [];
-        array.forEach(function(item){
-    output.push(item)
-})
+function mapWith(array, callback) {
+  var output = [];
+  array.forEach(function (item) {
+    output.push(item);
+  });
 }
 
-var multiply = function(){
-    return item * 2
-}
+var multiply = function () {
+  return item * 2;
+};
 
-var result = mapWith([1,2,3,4,5],multiply)
+var result = mapWith([1, 2, 3, 4, 5], multiply);
 
-doThisMath(5,square);//this will log out 25
+doThisMath(5, square); //this will log out 25
 
-doThisMath(4,increment);//this will log out 5
-
+doThisMath(4, increment); //this will log out 5
 
 //Challenge translation result
 
-const increment = n => {return n+1;};
-
-const square = n => {return n*n};
-
-const doThisMath = ( n, func ) => { return(func(n);};
-					   
-
-//					   
-function copy(array,instruction){
-var output  = [];
-for(i=0;i<array.length;i++){
-output.push(instruction(array[i]))
-}
-return output
-}
-
-var multiply = function(input){
-    return input*2
+const increment = (n) => {
+  return n + 1;
 };
 
-var result = copy([1,2,3],multiply)
+const square = (n) => {
+  return n * n;
+};
+
+const doThisMath = (n, func) => {
+  return func(n);
+};
+
+//
+function copy(array, instruction) {
+  var output = [];
+  for (i = 0; i < array.length; i++) {
+    output.push(instruction(array[i]));
+  }
+  return output;
+}
+
+var multiply = function (input) {
+  return input * 2;
+};
+
+var result = copy([1, 2, 3], multiply);
 
 //result = [2,4,6]
